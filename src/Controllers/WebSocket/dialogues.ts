779c353098019eceb38581dc1e7d7bde6e97e49a
maskpa100 <maskpa@yandex.ru>
2024-10-28 18:20:24 +0800

    j

--- src/Controllers/WebSocket/dialogues.ts
+++ src/Controllers/WebSocket/dialogues.ts
@@ -1,21 +1,28 @@
-import { getUnreadMessages, recentMessages } from "../../query_MySql";
+import { getUnreadMessages, getUsers, recentMessages } from "../../query_MySql";
+import { getUniqueUserIds } from "../../utils/getUniqueUserIds";
 import { groupMessages } from "../../utils/groupMessages";
 import { mergeMessages } from "../../utils/mergeMessages";
 
 export const unreadMessages = async (userId: number) => {
   const resultUnreadMessages = await getUnreadMessages(userId);
   const resultRecentMessages = await recentMessages(userId);
-
   const data = {
     userId: String(userId),
     messages: resultRecentMessages,
   };
-
   const resultGroupMessages = groupMessages(data);
   const resultMergeMessages = mergeMessages(
     resultGroupMessages,
     resultUnreadMessages,
     userId
   );
-  return resultMergeMessages;
+  const userIds = getUniqueUserIds(resultMergeMessages, userId);
+  const users = await getUsers(userIds);
+  const result = resultMergeMessages.map((dialog) => {
+    const user = users.find(
+      (user) => user.id === dialog.dialog_userId || user.id === dialog.userId
+    );
+    return { ...dialog, dialog_user: user || null };
+  });
+  return result;
 };
