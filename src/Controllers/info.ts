42e45e6ac1d0a16bcb0e4289ffde66cdb5ed0b01
maskpa100 <maskpa@yandex.ru>
2024-11-08 03:50:25 +0800

    8

---
+++ src/Controllers/info.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { AuthenticatedRequest } from "../types";
+import { JwtPayload } from "jsonwebtoken";
+import { query_MySql } from "../config/MySql";
+export const info = async (req: Request, res: Response): Promise<void> => {
+  const user = (req as AuthenticatedRequest).user;
+  const userId =
+    user && typeof user !== "string" && (user as JwtPayload).userId
+      ? (user as JwtPayload).userId
+      : null;
+
+  const getUserByIdSql =
+    "SELECT id, family,email name, avatar FROM users WHERE id = ?";
+
+  const resultUser = await query_MySql(getUserByIdSql, [userId]);
+  console.log(resultUser[0].avatar);
+
+  const userInfo = {
+    userId: resultUser[0].id,
+    username: resultUser[0].email,
+    family: resultUser[0].family,
+    name: resultUser[0].name,
+    avatar: resultUser[0].avatar,
+  };
+  res.status(200).json({ message: "Доступ разрешен", userInfo, valid: true });
+};
--- src/Routers/auth.ts
+++ src/Routers/auth.ts
@@ -5,6 +5,7 @@ import { Request, Response } from "express";
 import { AuthenticatedRequest } from "../types";
 import { JwtPayload } from "jsonwebtoken";
 import { query_MySql } from "../config/MySql";
+import { info } from "../Controllers/info";
 
 const authRouter = Router();
 
@@ -12,31 +13,6 @@ authRouter.post("/login", login);
 
 authRouter.post("/register", register);
 
-authRouter.get(
-  "/verify-token",
-  verifyToken,
-  async (req: Request, res: Response) => {
-    const user = (req as AuthenticatedRequest).user;
-    const userId =
-      user && typeof user !== "string" && (user as JwtPayload).userId
-        ? (user as JwtPayload).userId
-        : null;
-
-    const getUserByIdSql =
-      "SELECT id, family,email name, avatar FROM users WHERE id = ?";
-
-    const resultUser = await query_MySql(getUserByIdSql, [userId]);
-    console.log(resultUser[0].avatar);
-
-    const userInfo = {
-      userId: resultUser[0].id,
-      username: resultUser[0].email,
-      family: resultUser[0].family,
-      name: resultUser[0].name,
-      avatar: resultUser[0].avatar,
-    };
-    res.status(200).json({ message: "Доступ разрешен", userInfo, valid: true });
-  }
-);
+authRouter.get("/verify-token", verifyToken, info);
 
 export default authRouter;
