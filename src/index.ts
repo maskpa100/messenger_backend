42dfed673f5b0e890aee8034718f04c652bc7058
maskpa100 <maskpa@yandex.ru>
2024-11-13 17:29:12 +0800

    j

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
 import express from "express";
-import WebSocket, { WebSocketServer } from "ws";
-import jwt, { JwtPayload } from "jsonwebtoken";
 import cors from "cors";
 import authRouter from "./Routers/auth";
 import messengersRouter from "./Routers/messengers";
 import userRouter from "./Routers/user";
-import { SECRET_KEY } from "./config/authSecret";
 import { initWebSocketServer } from "./Routers/WebSocket";
 import settingRouter from "./Routers/setting";
 import { searchUsers } from "./Controllers/searchUsers";
 const path = require("path");
-import https from "https";
-import fs from "fs";
+require("dotenv").config({ path: ".env.local" });
+console.log(
+  process.env.HOST,
+  process.env.USER,
+  process.env.PASSWORD,
+  process.env.DATABASE
+);
 
 const app = express();
 const port = 5000;
-
-const sslOptions = {
-  key: fs.readFileSync("./server.key"), // Замените на путь к вашему файлу server.key
-  cert: fs.readFileSync("./server.cert"), // Замените на путь к вашему файлу server.cert
-};
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -35,6 +32,6 @@ app.get("/search", searchUsers);
 
 initWebSocketServer(8081);
 
-https.createServer(sslOptions, app).listen(port, () => {
-  console.log(`Example app listening on HTTPS port ${port}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
